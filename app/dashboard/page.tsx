dc1744cb9c4fb816af0debf73f08ee81adf6b41a
Paulo <pdichone@gmail.com>
2024-01-17 16:56:02 -0800

    Setup radix ui

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes'
+
+const Dashboard = () => {
+  return (
+    <div className='flex mx-5'><Button>Hello</Button></div>
+  )
+}
+
+export default Dashboard
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+import'@radix-ui/themes/styles.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './NavBar'
 
+import {Theme} from '@radix-ui/themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,10 +22,10 @@ export default function RootLayout({
     <html lang="en">
       
       <body className={inter.className}>
-      <NavBar />
-      <main>
-        {children}
-      </main>
+        <Theme>
+          <NavBar />
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   )
